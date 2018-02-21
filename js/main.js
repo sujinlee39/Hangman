0076b3ca054e77175e0a5b87ac6a5002d7161a09
Sujin Lee <sujinlee@sujins-mbp-2.fc.ca>
2018-02-21 16:41:56 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 (() => {
+  // variables always go at the top of the files
   var words = ["animals", "food", "music genres", "movie character", "countries", "popular movies"];
 
   let currentWord = words[Math.floor(Math.random()*words.length)],
-  wordHint = document.querySelector('.current-word');
+  wordHint = document.querySelector('.current-word'),
+  guessBox = document.querySelector('.letter-guess');
 
   wordHint.textContent = currentWord.split("").map(letter => letter = ("__")).join(" ");
 
-  debugger;
+  //unctions go in the middle (logic, etc)
+  function makeGuess() {
+    //debugger;
+    console.log(this.value);
+  }
+
+  // event handling goes at the bottom
+guessBox.addEventListener('keyup', makeGuess);
 
   // storing the random workd in a variable
 
